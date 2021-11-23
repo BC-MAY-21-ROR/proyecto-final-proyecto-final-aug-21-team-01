12536bb93c0e311aabe5735a5ec3ac684f970d57
Jose Estrada <j.estrada.developer@gmail.com>
2021-11-22 18:35:39 -0600

    Add Category Model
    This commit adds a category model and a craate categories migration in order to have an entity to store what kind of group are being created by the users.
    
    Co-authored-by: Juanequex <ramon.g.uz37@gmail.com>
    Co-authored-by: CUBOFIG <hfigueroa@ucol.mx>
    Co-authored-by: adrianMRJ <adrian.mrj.97@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_225213) do
+ActiveRecord::Schema.define(version: 2021_11_23_002429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_categories_on_group_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -36,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_11_17_225213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "groups"
 end
